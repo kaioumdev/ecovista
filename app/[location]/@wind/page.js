bd4be136fb41022b47d97ef1ce9368c4c42beaed
kaiyumdev <afrankaiyum34@gmail.com>
2025-07-25 20:05:49 +0600

    complete windPage successfully

--- app/[location]/@wind/page.js
+++ app/[location]/@wind/page.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import NoLocationInfo from "@/components/NoLocationInfo";
+import WindComponent from "@/components/WindComponent";
+import { getResolvedLatLong } from "@/lib/location-info";
 
-const page = () => {
-    return (
-        <div>Wind Page</div>
-    )
-}
+const WindPage = async ({
+    params: { location },
+    searchParams: { latitude, longitude },
+}) => {
+    const resolved = await getResolvedLatLong(location, latitude, longitude);
 
-export default page
+    if (resolved?.lat && resolved?.lon) {
+        return <WindComponent lat={resolved.lat} lon={resolved.lon} />;
+    } else {
+        return <NoLocationInfo />;
+    }
+};
+
+export default WindPage;
