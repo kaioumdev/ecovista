a3839b3efb81235fc88fe3d4c9ad9f9cfdcbe9b9
kaiyumdev <afrankaiyum34@gmail.com>
2025-07-25 20:03:07 +0600

    complete weatherPage successfully

--- app/[location]/@weather/page.js
+++ app/[location]/@weather/page.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import NoLocationInfo from '@/components/NoLocationInfo';
+import WeatherComponent from '@/components/WeatherComponent';
+import { getResolvedLatLong } from '@/lib/location-info';
 
-const page = () => {
-    return (
-        <div>Weather Page</div>
-    )
-}
+const CurrentWeatherPage = async ({
+    params: { location },
+    searchParams: { latitude, longitude },
+}) => {
+    const resolved = await getResolvedLatLong(location, latitude, longitude);
 
-export default page
+    if (resolved?.lat && resolved?.lon) {
+        return <WeatherComponent lat={resolved.lat} lon={resolved.lon} />;
+    } else {
+        return <NoLocationInfo />;
+    }
+};
+
+export default CurrentWeatherPage;
