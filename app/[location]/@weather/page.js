cc3ed63452318229baf91bc7691b0fba8f2aa9f8
kaiyumdev <afrankaiyum34@gmail.com>
2025-07-25 17:57:07 +0600

    location detector page work successfully to detect the location

--- app/[location]/@weather/page.js
+++ app/[location]/@weather/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div>Weather page</div>
+        <div>Weather Page</div>
     )
 }
 
--- app/[location]/layout.js
+++ app/[location]/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "../globals.css";
+import Image from "next/image";
 
 
 const inter = Inter({
@@ -11,11 +12,27 @@ export const metadata = {
     description: "One Place Dashboard for Eco Information",
 };
 
-export default function RootLayout({ children, aqi }) {
+export default function RootLayout({ children, weather, aqi, temperature, wind }) {
     return (
         <div className="wrapper">
-            {children}
-            {aqi}
+            <div className="overlay"></div>
+            <Image
+                src="/background.png"
+                className="bg-img"
+                width={700}
+                height={1200}
+            />
+            <main className="!z-50 w-full">
+                <div className="container">
+                    <div className="grid grid-cols-12 gap-y-8 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
+                        {children}
+                        {weather}
+                        {aqi}
+                        {wind}
+                        {temperature}
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
